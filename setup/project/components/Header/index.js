ba3a52ca784392c1585a984f575b433f9d08790d
Asim <asimwebapps@gmail.com>
2021-12-13 23:58:32 +0300

    added Appbar from react-native-paper

--- setup/project/components/Header/index.js
+++ setup/project/components/Header/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { SafeAreaView, View, Text } from 'react-native';
+import { Platform } from 'react-native';
+import { Appbar } from 'react-native-paper';
 import styles from './styles'
+
+
+const MORE_ICON = Platform.OS === 'ios' ? 'dots-horizontal' : 'dots-vertical';
 
 
 export default function Header(props) {
     return (
-        <View style={styles.root}>
-            <SafeAreaView>
-                <View style={styles.view}>
-                    <Text>aaaa</Text>
-                    <Text>{props.page_name}</Text>
-                    <Text>cccc</Text>
-                </View>
-            </SafeAreaView>
-        </View>
+        <Appbar.Header>
+
+            <Appbar.BackAction onPress={() => { }} />
+
+            <Appbar.Content title={props.page_name} subtitle={props.page_name} />
+
+            <Appbar.Action icon="magnify" onPress={() => { }} />
+
+            <Appbar.Action icon={MORE_ICON} onPress={() => { }} />
+
+        </Appbar.Header>
     );
 }
