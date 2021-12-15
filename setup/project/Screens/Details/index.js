5d994b4b4e708de6400f049d17fe1b05dad3e27a
Asim <asimwebapps@gmail.com>
2021-12-16 00:14:43 +0300

    update home navigation links

--- setup/project/Screens/Details/index.js
+++ setup/project/Screens/Details/index.js
@@ -7,14 +7,14 @@ import Screen from "../Base";
 export default function DetailsScreen(props) {
     return (
         <Screen
-            home_link={() => props.navigation.jumpTo('Navigator 2')}
+            home_link={() => props.navigation.jumpTo('Home')}
             back_link={() => props.navigation.jumpTo('Navigator 2')}
             title="Details Title">
 
 
             <Text>Details Screen</Text>
 
-            <Button title="Home" onPress={() => props.navigation.jumpTo('Navigator 2')} />
+            <Button title="Go to Home Settings" onPress={() => props.navigation.navigate('Home', { screen: "Settings" })} />
 
         </Screen>
     );
